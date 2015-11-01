6cdda2a5e69334a820fada621e923b7172aa30ca
V1C0D3R <V1C0D3R@gmail.com>
2015-11-01 19:24:20 +0100

    Add Consolidate package

--- app.js
+++ app.js
@@ -4,21 +4,27 @@
 var express = require('express');
 var app     = express();
 var path    = require('path');
+var cons    = require('consolidate');
 
-//Store all HTML files in pages folder.
-app.use(express.static(__dirname + '/pages'));
+//Render Engine
+app.engine('html', cons.swig);
 
-var apiRequest = require(path.join(__dirname+'/jcdecaux_api/api.requests.js'));
-var config     = require(path.join(__dirname+'/config.js'));
+// set .html as the default extension
+app.set('view engine', 'html');
+app.set('views', __dirname + '/pages');
+
+var apiRequest  = require(path.join(__dirname+'/jcdecaux_api/api.requests.js'));
+var config      = require(path.join(__dirname+'/config.js'));
 
 //Every URL access are redirected here :
 
 app.get('/', function (req, res) {
-  res.sendFile('/index.html');
+  res.render('index');
+  //res.sendFile('/index.html');
 });
 
 app.get('/about', function (req, res) {
-  res.redirect('/about.html');
+  res.render('about');
 });
 
 app.get('/sayHello', function (req, res) {
